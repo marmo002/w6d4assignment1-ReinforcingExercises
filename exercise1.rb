1bad787f8d5846b3d029b1ea0398c215274db7a0
marmo002 <marmo_02@hotmail.com>
2017-10-22 14:30:12 -0400

    Finish exercise

--- exercise1.rb
+++ exercise1.rb
@@ -5,14 +5,32 @@ seats = [
   ["Smokey","Toast","Pacha","Mau"]
 ]
 
-# p seats
+#PART 1
+# seats.each_with_index  do |item, index|
+#   index += 1
+#   item.each_with_index do |v, i|
+#     i += 1
+#     if v == nil
+#       puts "Row #{index} seat #{i} is free"
+#     end
+#   end
+# end
 
+#PART 2
 seats.each_with_index  do |item, index|
-  index += 1
+  index
   item.each_with_index do |v, i|
-    i += 1
+    i
     if v == nil
-      puts "Row #{index} seat #{i} is free"
+      puts "Row #{index + 1} seat #{i+1} is free. Do you wanna seat there? (y/n)"
+      input = gets.chomp
+      if input == "y"
+        puts "What's your name?"
+        name = gets.chomp
+        seats[index][i] = name
+        p seats
+        exit
+      end
     end
   end
 end
